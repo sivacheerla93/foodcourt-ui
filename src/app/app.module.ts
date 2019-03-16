e822ffce72a3a303a91cac44ee82c9fa5b8b07fa
Siva Cheerla <sivacheerla@live.com>
2019-03-16 21:41:45 +0530

    home page design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule, routing],
+  declarations: [AppComponent, HomeComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
