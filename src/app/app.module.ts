697b5220d5535f66b5215f7083e42d779e616063
Siva Cheerla <sivacheerla@live.com>
2019-03-17 15:53:08 +0530

    17/03/2019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 
 import { HomeComponent } from './home/home.component';
+import { OrderComponent } from './consumer/order.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule, routing],
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, OrderComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { OrderComponent } from './consumer/order.component';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'order', component: OrderComponent }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
