56c1185447271474b631f1269b1539f6be9a11ac
Siva Cheerla <sivacheerla@live.com>
2019-03-18 02:07:49 +0530

    18/03/2019

--- src/app/consumer/order.component.ts
+++ src/app/consumer/order.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
     selector: 'order-root',
     templateUrl: './order.component.html',
     styleUrls: ['./order.component.css']
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
     title = 'Home';
+
+    ngOnInit() {
+        // For quantity spinner
+        $(document).ready(function () {
+            $('.count').prop('disabled', true);
+            $(document).on('click', '.plus', function () {
+                $('.count').val(Number($('.count').val()) + 1);
+            });
+            $(document).on('click', '.minus', function () {
+                $('.count').val(Number($('.count').val()) - 1);
+                if ($('.count').val() == 0) {
+                    $('.count').val(1);
+                }
+            });
+        });
+    }
+
+
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+//import * as $ from 'jquery';
 
 @Component({
     selector: 'home-root',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
     title = 'Home';
-    $: any;
 
-    // ngOnInit() {
-    //     $(document).ready(function () {
-    //         $('.carousel').carousel();
-    //     })
-    // }
+    ngOnInit() {
+        $(document).ready(function () {
+            ($('.carousel') as any).carousel();
+        })
+    }
 }
 
